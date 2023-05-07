05cd9df21066e0042a1ab3d4e94537962050582c
zAxOzXVIII <119118178+zAxOzXVIII@users.noreply.github.com>
2023-05-07 16:54:41 -0400

    Previa_actualizacion
    funcionalidad de qr y guardado de datos

--- App_Create_QR/index.py
+++ App_Create_QR/index.py
@@ -1,5 +1,5 @@
 # Codigo 
-import qrcode as qrc
+import qrcode
 from tkinter import *
 from tkinter import ttk
 import csv
@@ -7,12 +7,12 @@ import csv
 # App
 class App_Qr:
     def __init__(self, window):
-        # window
+        # window Principal
         self.window = window
         self.window.title("Qr Codes")
         # LabelFrame
-        frame = LabelFrame(self.window, text="Queso_chedar", pady=100, padx=100)
-        frame.grid(column=0, row=0, columnspan=3, pady=5, padx=5)
+        frame = LabelFrame(self.window, text="Ingresar Data", pady=60, padx=60)
+        frame.grid(column=0, row=0, columnspan=3)
         # Code Input
         Label(frame, text="Codigo de producto").grid(column=0, row=0)
         self.code = Entry(frame)
@@ -22,8 +22,21 @@ class App_Qr:
         self.desc = Entry(frame)
         self.desc.grid(column=1, row=1, pady=15)
         # Button
-        self.button = ttk.Button(frame, text="Guardar", command=self.save_data())
-        self.button.grid(column=0, row=2, columnspan=2, sticky=W + E)
+        self.button = ttk.Button(frame, text="Guardar", command= lambda : self.save_data())
+        self.button.grid(column=0, row=2, columnspan=2, sticky=W + E, pady=25)
+        # Tabla
+        
+        self.tree = ttk.Treeview(self.window, height=10, columns=2)
+        self.tree.grid(column=0, row=3, columnspan=2)
+        self.tree.heading("#0", text="Code", anchor=CENTER)
+        self.tree.heading("#1", text="Description", anchor=CENTER)
+        
+        # Button _Table
+        self.button_table = ttk.Button(self.window, text="Generar Codigo Qr", command= lambda : self.convert_Qr())
+        self.button_table.grid(column=0, row=4, columnspan=2, sticky = W+E, pady=5)
+        
+        # filling tables
+        self.get_products()
     
     def save_data(self):
         notes = self.read_data()
@@ -31,13 +44,15 @@ class App_Qr:
         # llenando data
         if self.input_vacio():
             data = [self.code.get(), self.desc.get()]
+            notes.append(data)
         else: print("Debe llenar las casillas")
         # save new data
-        notes.append(data)
         
         with open("Python-Projects\\App_Create_QR\\storage\\almacen\\data.csv", mode="w", newline="") as archive:
             writer = csv.writer(archive, delimiter=",")
             writer.writerows(notes)
+        # load page
+        self.get_products()
     
     def read_data(self):
         
@@ -47,8 +62,45 @@ class App_Qr:
             return notes
     
     def input_vacio(self):
-        if ((self.desc.get() == "") & (self.code.get() == "")): return False
-        else: return True
+        if ((self.desc.get() != "") & (self.code.get() != "")): return True
+        else: return False
+    
+    def get_products(self):
+        # Cleaning table
+        records = self.tree.get_children()
+        for element in records:
+            self.tree.delete(element)
+        # get data
+        notes = self.read_data()
+        print(notes)
+        for column in range(1, len(notes)):
+            self.tree.insert("", 0, text=notes[column][0], values=notes[column][1])
+    
+    def select_item(self):
+        try:
+            self.tree.item(self.tree.selection())["text"][0]
+        except IndexError as e:
+            print("Error no se ha seleccionado ningun dato")
+            return
+        code = self.tree.item(self.tree.selection())["text"]
+        name_image = str(self.tree.item(self.tree.selection())["values"])
+        print(code)
+        print(self.tree.item(self.tree.selection()))
+        return code, name_image
+    
+    def convert_Qr(self):
+        code, name_image= self.select_item()
+        qr = qrcode.QRCode(
+            version=5,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=15,
+            border=6,
+        )
+        qr.add_data(code)
+        qr.make(fit=True)
+        
+        img = qr.make_image(fill_color="black", back_color="white")
+        img.save("Python-Projects\\App_Create_QR\\storage\\images\\img_"+ name_image +".png")
 
 
 # Objeto
