9fc1301bdb48cd6ff4827eb5ea4e6ccaa9e5e943
zAxOzXVIII <119118178+zAxOzXVIII@users.noreply.github.com>
2023-06-10 21:45:03 -0400

    Actualizacion 1.1.0.0
    Formato de programa con las 3 sesiones
    CRUD de administrador

--- Bienes_Muebles_T2/index.py
+++ Bienes_Muebles_T2/index.py
@@ -21,6 +21,7 @@ class Window:
     db_name = "bienes_muebles"
     def __init__(self, window):
         self.window = window
+        print(type(self.window))
         self.window.title("Bienes Muebles Kino Tachira")
         
         # Label Titulo Main
@@ -45,27 +46,50 @@ class Window:
         self.pie_pagina()
         
     
-    def limpiar_ventana(self):
+    def limpiar_ventana(self, opc = 1):
         # limpiando los widgets
-        for widget in self.window.winfo_children():
-            widget.destroy()
+        if opc == 1:
+            for widget in self.window.winfo_children():
+                widget.destroy()
+        elif opc == 2:
+            for widget in self.toplevel_admin_window.winfo_children():
+                widget.destroy()
     
-    def run_query(self, query, parameters=()):
-        try:
-            conn = mdb.connect(host="127.0.0.1", user="root", 
+    def run_query(self, query, parameters=(), opc = 1):
+        if opc == 1:
+            try:
+                conn = mdb.connect(host="127.0.0.1", user="root", 
                                 password="", 
                                 database=self.db_name)
-            cursor = conn.cursor()
-            cursor.execute(query, parameters)
-            conn.commit()
-            fetch = cursor.fetchall()
-            return fetch
-        except mdb.Error as e:
-            message = f"Alert Error: {e}"
-            return message
-        finally:
-            conn.close()
-            print("Se ha finalizado la conexion")
+                cursor = conn.cursor()
+                cursor.execute(query, parameters)
+                conn.commit()
+                fetch = cursor.fetchall()
+                print(fetch, "l62")
+                return fetch
+            except mdb.Error as e:
+                message = f"Alert Error: {e}"
+                return message
+            finally:
+                conn.close()
+                print("Se ha finalizado la conexion")
+        elif opc == 2:
+            try:
+                conn = mdb.connect(host="127.0.0.1", user="root", 
+                                password="", 
+                                database=self.db_name)
+                cursor = conn.cursor()
+                cursor.execute(query, parameters)
+                conn.commit()
+                rowcount = cursor.rowcount()
+                print(rowcount, "l62")
+                return rowcount
+            except mdb.Error as e:
+                message = f"Alert Error: {e}"
+                return message
+            finally:
+                conn.close()
+                print("Se ha finalizado la conexion")
     
     def obtener_directorio(self):
         directorio = os.getcwd()
@@ -114,7 +138,7 @@ class Window:
         
         label_panel = Label(self.window, text="Panel de Usuarios").grid(row=0, column=0, columnspan=2)
         Label(self.window, text="Sesion Administrador").grid(row=1, column=0)
-        boton_admin = ttk.Button(self.window, text="Iniciar Sesion").grid(row=1, column=1)
+        boton_admin = ttk.Button(self.window, text="Iniciar Sesion", command = self.toplevel_login_admin).grid(row=1, column=1)
         
         Label(self.window, text="Sesion supervisor").grid(row=2, column=0)
         boton_sup = ttk.Button(self.window, text="Iniciar Sesion", command=self.toplevel_login_sup).grid(row=2, column=1)
@@ -130,6 +154,7 @@ class Window:
         
         try:
             fetch = self.run_query(sql, (self.code.get(), ))
+            print(fetch, "l138")
             # imprimiendo valores
             for row in fetch:
                 name = row[0]
@@ -193,11 +218,15 @@ class Window:
         sql = """INSERT INTO bienes_por_zona(id_area, cantidad, num_cons, desc_item, valor, observacion)
                 VALUES(?, ?, ?, ?, ? ,?)"""
         parameters = (id, cnt, nc, di, vlr, obs)
-        self.run_query(sql, parameters)
-        # limpiando formulario
-        messagebox.showinfo(title="Mensaje del sistema",
-                            message="Se subio correctamente los datos")
+        fetch =  self.run_query(sql, parameters)
         
+        # limpiando formulario
+        if fetch > 0:
+            messagebox.showinfo(title="Mensaje del sistema",
+                            message="Se subio correctamente los datos")
+        else:
+            messagebox.showwarning("Advertencia del sistema", "Error al subir la data")
+            return
         self.cantidad.delete(0, END)
         self.num_cons.delete(0, END)
         self.desc_item.delete(0, END)
@@ -442,10 +471,235 @@ class Window:
     
     def w_admin_main(self, user):
         # Asignando a main
-        self.window.geometry("200x200")
+        # self.window.geometry("200x200")
         # Mensajes label
-        Label(self.window, text=f"Bienvenido {user}").pack()
+        Label(self.window, text=f"Bienvenido {user}").grid(row=0, column=0, columnspan=3, pady=15)
+        # Mostrando los supervisores en turno
+        Label(self.window, text="Tabla informativa").grid(row=1, column=0, padx=5)
+        self.admin_table_sup = ttk.Treeview(self.window, height=10)
+        # Asignando lugar
+        self.admin_table_sup.grid(row=1, column=1, columnspan=2, padx=5)
+        # especificando columnas
+        self.admin_table_sup["columns"] = ("c1", "c2", "c3")
+        # Asignando nombres a las columnas
+        self.admin_table_sup.heading(column= "c1", text="Nombre")
+        self.admin_table_sup.heading(column= "c2", text="Apellido")
+        self.admin_table_sup.heading(column= "c3", text="Cedula")
+        # Ajustar columnas al contenido
+        for columna in self.admin_table_sup["columns"]:
+            self.admin_table_sup.column(columna, width=100, minwidth=50, anchor=CENTER)
+        # Llenando tabla
+        self.fill_admin_table_sup()
+        # Botones
+        ttk.Button(self.window, text="Agregar supervisor", command = lambda : self.topLevel_admin_interface_add()).grid(row=2, column=0)
+        ttk.Button(self.window, text="Modificar Supervisor", command = lambda : self.topLevel_admin_interface_update()).grid(row=2, column=1)
+        ttk.Button(self.window, text="Eliminar Supervisor", command = lambda : self.topLevel_admin_interface_delete()).grid(row=2, column=2)
+        # Consultar usuarios admin
+        Label(self.window, text="Consultar sobre los usuarios admin disponibles").grid(row=3, column=1, columnspan=2, pady=15)
+        ttk.Button(self.window, text="Usuarios Admin", command = self.topLevel_admin_i).grid(row=3, column=0, pady= 15)
+        # pie de pagina
+        self.pie_pagina()
     
+    def fill_admin_table_sup(self, busqueda = (), w_quest = "1"):
+        # limpiamos tabla
+        records = self.admin_table_sup.get_children()
+        print (records)
+        for element in records:
+            self.admin_table_sup.delete(element)
+        
+        busqueda_personalizada = f"{w_quest}"
+        print(busqueda, "\n", busqueda_personalizada)
+        if len(busqueda) <= 0:
+            query = f"SELECT * FROM supervisor_area_kino WHERE {busqueda_personalizada}"
+            fetch = self.run_query(query)
+            for row in fetch:
+                self.admin_table_sup.insert("", "end", text=row[0], values = (row[1], row[2], row[3]))
+        else:
+            for data in busqueda:
+                valor = busqueda_personalizada + data
+                print(valor)
+                query = f"SELECT * FROM supervisor_area_kino WHERE {valor}"
+                fetch = self.run_query(query)
+                for row in fetch:
+                    self.admin_table_sup.insert("", "end", text=row[0], values = (row[1], row[2], row[3]))
+                
+    # Consulta si se ha seleccionado una fila de la tabla de supervisores
+    def select_admin_table_sup(self):
+        try:
+            self.admin_table_sup.item(self.admin_table_sup.selection())["text"]
+        except IndexError as e:
+            messagebox.showwarning("Advertencia del sistema", e)
+            return
+        
+        name = self.admin_table_sup.item(self.admin_table_sup.selection())["values"][0]
+        last_name = self.admin_table_sup.item(self.admin_table_sup.selection())["values"][1]
+        dni = self.admin_table_sup.item(self.admin_table_sup.selection())["values"][2]
+        return (name, last_name, dni)
+    
+    def topLevel_admin_interface_update(self):
+        update_window = Toplevel()
+        update_window.title("Agregar datos ventana")
+        # Datos anteriores
+        old_data = self.select_admin_table_sup()
+        if len(old_data) <= 0:
+            messagebox.showwarning("Advertencia del sistema", "Se debe seleccionar un registro")
+            return
+        
+        # agregando labels
+        Label(update_window, text=f"Antigua información : {old_data[0]}").grid(row=0, column=0)
+        Label(update_window, text="Nombre del supervisor\nQue se actualizará").grid(row=1, column=0)
+        name = Entry(update_window)
+        name.grid(row=1, column=1)
+        
+        Label(update_window, text=f"Antigua información : {old_data[1]}").grid(row=2, column=0)
+        Label(update_window, text="Apellido del supervisor\nQue se actualizará").grid(row=3, column=0)
+        last_name = Entry(update_window)
+        last_name.grid(row=3, column=1)
+        
+        Label(update_window, text=f"Antigua información : {old_data[2]}").grid(row=4, column=0)
+        Label(update_window, text="Cedula del supervisor\nQue se actualizará").grid(row=5, column=0)
+        dni = Entry(update_window)
+        dni.grid(row=5, column=1)
+        
+        # query
+        ttk.Button(update_window, text="Actualizar", command = lambda: self.admin_query_customThree(2, old_data,
+                                                                                                        (name.get(), last_name.get(), dni.get()))).grid(row= 6, column=0, columnspan=2)
+        # pie de pagina
+        self.pie_pagina()
+    
+    def admin_query_customThree(self, opc : int, old_info = (), new_info = ()):
+        # imprimiendo valores
+        print("Valores old_info L533", old_info)
+        print("Valores new_info L534", new_info)
+        if opc == 1:
+            query = "INSERT INTO supervisor_area_kino(nombre, apellido, cedula) VALUES(?, ?, ?)"
+            fetch = self.run_query(query, new_info)
+            if len(fetch) <= 0:
+                print("la insercion salio bien")
+            else: print(fetch)
+        elif opc == 2:
+            query = f"UPDATE supervisor_area_kino SET nombre = ?, apellido = ?, cedula = ? WHERE nombre = {old_info[0]} AND cedula = {old_info[2]}"
+            fetch = self.run_query(query, new_info)
+            if len(fetch) <= 0: print("el update salio bien", fetch)
+        elif opc == 3:
+            query = f"DELETE FROM supervisor_area_kino WHERE nombre = ? AND apellido = ? AND cedula = ?"
+            fetch = self.run_query(query, old_info)
+            print(fetch, "Mensaje L555")
+        
+    
+    def topLevel_admin_interface_add(self):
+        # ventana add
+        add_window = Toplevel()
+        print(type(add_window))
+        add_window.title("Ventana de agregar")
+        # Labels
+        Label(add_window, text="Nombre del supervisor nuevo").grid(row=0, column=0)
+        name = Entry(add_window)
+        name.grid(row=0, column=1)
+        
+        Label(add_window, text="Apellido del supervisor nuevo").grid(row=1, column=0)
+        last_name = Entry(add_window)
+        last_name.grid(row=1, column=1)
+        
+        Label(add_window, text="Cedula del supervisor nuevo").grid(row=2, column=0)
+        dni = Entry(add_window)
+        dni.grid(row=2, column=1)
+        # data
+        # boton
+        btn = ttk.Button(add_window, text="Agregar valores", command = lambda : self.admin_query_customThree(1, (),
+                                                                                                                (name.get(), last_name.get(), dni.get())))
+        btn.grid(row=3, column = 0, columnspan=2)
+    
+    def topLevel_admin_interface_delete(self):
+        # Llamando query para eliminar data
+        select_data = self.select_admin_table_sup()
+        if len(select_data) > 0: 
+            if messagebox.askyesno("Pregunta del sistema", f"Esta seguro de eliminar el registro {select_data}"):
+                self.admin_query_customThree(3, old_info = select_data)
+        else:
+            messagebox.showwarning("Advertencia del sistema", "Error, debe seleccionar una fila")
+            return
+        # Actualizando tabla
+        self.fill_admin_table_sup()
+        
+    
+    def topLevel_admin_i(self):
+        self.toplevel_admin_window = Toplevel()
+        self.toplevel_admin_window.title("Panel de admins")
+        # limpiar ventana
+        self.limpiar_ventana(opc = 2)
+        # Labels
+        Label(self.toplevel_admin_window, text= "Administradores disponibles").grid(row= 0, column=0, columnspan=2)
+        # Tabla
+        self.table_admin_data = ttk.Treeview(self.toplevel_admin_window, height=10)
+        # asignando a grilla
+        self.table_admin_data.grid(row=1, column=0, columnspan=2)
+        # columnas
+        self.table_admin_data["columns"] = ("col0", "col1")
+        # asignando
+        self.table_admin_data.heading(column = "col0", text = "Usuario")
+        self.table_admin_data.heading(column = "col1", text = "Contraseña")
+        # asignando espacio
+        
+        # asignando valores a las filas de la tabla admins
+        self.fill_admin_table()
+        # Botones
+        ttk.Button(self.toplevel_admin_window, text="Agregar", command = self.toplevel_admin_addW).grid(row=2, column=0)
+        ttk.Button(self.toplevel_admin_window, text="Eliminar", command = self.toplevel_admin_deleteW).grid(row=2, column=1)
+        
+    
+    def fill_admin_table(self):
+        # limpiamos tabla
+        records = self.table_admin_data.get_children()
+        print (records)
+        for element in records:
+            self.table_admin_data.delete(element)
+        
+        query = "SELECT * FROM administrador"
+        fetch = self.run_query(query)
+        print(fetch, type(fetch))
+        if isinstance(fetch, list):
+            for row in fetch:
+                self.table_admin_data.insert("", "end", text=row[0], values = (row[1], row[2]))
+        else: messagebox.showwarning("Advertencia del sistema", fetch)
+    
+    def toplevel_admin_addW(self):
+        # Limpiar ventana
+        self.limpiar_ventana(opc = 2)
+        # Formulario
+        Label(self.toplevel_admin_window, text="Ingresar Usuario").grid(row=0, column=0)
+        user = ttk.Entry(self.toplevel_admin_window)
+        user.grid(row=0, column=1)
+        Label(self.toplevel_admin_window, text="Ingresar Contraseña").grid(row=1, column=0)
+        password = ttk.Entry(self.toplevel_admin_window)
+        password.grid(row=1, column=1)
+        # Volver
+        ttk.Button(self.toplevel_admin_window, text="Volver", command = self.topLevel_admin_i).grid(row=2, column=0)
+        # Agregar
+        ttk.Button(self.toplevel_admin_window, text="Subir", command = lambda : self.add_admin_registers(user.get(),
+                                                                                                            password.get())).grid(row=2, column=1)
+    
+    def add_admin_registers(self, parameters = ()):
+        query = "INSERT INTO administrador(usuario, contraseña) VALUES(?, ?)"
+        rowcount = self.run_query(query, parameters, 2)
+        print("Numero de filas apectadas {rowcount}".format(rowcount))
+    
+    def toplevel_admin_deleteW(self):
+        try:
+            self.table_admin_data.item(self.table_admin_data.selection())["text"]
+        except IndexError as e:
+            messagebox.showwarning("Advertencia del sistema", e)
+            return
+        
+        user = self.table_admin_data.item(self.table_admin_data.selection())["values"][0]
+        password = self.table_admin_data.item(self.table_admin_data.selection())["values"][1]
+        old_data = (user, password)
+        
+        query = "DELETE FROM administrador WHERE usuario = ? AND contraseña = ?"
+        rowcount = self.run_query(query, old_data, 2)
+        if isinstance(rowcount, int): messagebox.showinfo("Mensaje del sistema", f"Se borraron: {rowcount} registros correctamente")
+
+# Proceso principal
 if __name__ == "__main__":
     # Inicializacion de la interfaz Tk
     window = Tk()
